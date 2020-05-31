16e030730c45f4f77a19cd14603a97ab724129b0
darkfantasy88 <59861596+darkfantasy88@users.noreply.github.com>
2020-05-30 22:24:21 -0500

    Shopping Cart

---
+++ js/shop.js
@@ -0,0 +1,95 @@
+let cartItems=[
+    {
+        name:"Americano",
+        price:250.00,
+        image:"food_items/americano(250).jpg"
+    },{
+        name:"Black Coffee",
+        price:200.00,
+        image:"food_items/blackcoffee 200.jpg"
+    },{
+        name:"Cappucino",
+        price:150.00,
+        image:"food_items/cappucino (150).jpg"
+    },{
+        name:"Chocolate-Berry Latte",
+        price:250.00,
+        image:"food_items/cd-chocolaterasberrylatte(250).jpg"
+    },{
+        name:"Hazelnut Latte",
+        price:200.00,
+        image:"food_items/cd-hazelnut latte(200).jpg"
+    },{
+        name:"Pepermint Latte",
+        price:300.00,
+        image:"food_items/cd-peppermintlatte(300).jpg"
+    },{
+        name:"Doppio",
+        price:290.00,
+        image:"food_items/Doppio(290).jpeg"
+    }
+    ,{
+        name:"Oreo Cake",
+        price:800.00,
+        image:"food_items/oreocake(800).jpg"
+    }
+    ,{
+        name:"Rasberry Blueberry Cheesecake",
+        price:600.00,
+        image:"food_items/RasberryBlueberryCheesecake(600).jpg"
+    }
+    ,{
+        name:"Red-Velvet Cake",
+        price:750.00,
+        image:"food_items/redvelvet(750).jpg"
+    }
+    ,{
+        name:"Strawberry Cheesecake",
+        price:750.00,
+        image:"food_items/StrawberryCheesecake(750).jpg"
+    },{
+        name:"Vanilla Fruit Cake",
+        price:500.00,
+        image:"food_items/VanillaFruitCake.jpg"
+    }
+]
+
+$().ready(function () {
+    LoadBuyItems();
+});
+
+function LoadBuyItems() {
+    for (let index = 0; index < cartItems.length; index++) {
+        var itemContainer=document.createElement("div");
+        itemContainer.className="sale-item";
+        var itemImg=document.createElement("img");
+        itemImg.src=cartItems[index].image;
+        var div=document.createElement("div");
+        var h5=document.createElement("h5");
+        h5.innerHTML=cartItems[index].name+" "+"( $"+cartItems[index].price+")";
+        div.appendChild(h5);
+        var btn=document.createElement("button");
+        btn.innerHTML="Add";
+        btn.setAttribute('onClick','AddCart('+index+')');
+        div.appendChild(btn);
+        itemContainer.appendChild(itemImg);
+        itemContainer.appendChild(div);
+        $(".item-container").append(itemContainer);
+        
+    }
+}
+
+function AddCart(index){
+    var item=cartItems[index];
+    var li=document.createElement("li");
+    li.setAttribute('value',item.price);
+    li.setAttribute('onClick','RemoveItem('+li+')');
+
+    li.innerHTML=item.name;
+    $("#cartlist").append(li);
+}
+
+function RemoveItem(listItem) {
+    $("#cartlist").remove(listItem);
+
+}
